903cdc0e88dcd6ccd72ba5d2cd0209fea9fe7051
Xinran <huxinran427@gmail.com>
2017-12-16 10:20:19 -0500

    a

---
+++ neuron.py
@@ -0,0 +1,15 @@
+class Neuron:
+    def __init__(self, inputDim, activateFunc):
+        self.inputDim = inputDim
+        self.activateFunc = activateFunc
+        self.weight = np.zeros(inputDim + 1, 1)
+        self.value = 0.0
+
+    def forward(input):
+        self.value = max(0, np.inner(input, self.weight))
+        return self.value
+    
+    def backward(gradient):
+        
+
+    
