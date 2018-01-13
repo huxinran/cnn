f5c76a2e0732c4a18754ebb8a30db5b5142511d3
Xinran <huxinran427@gmail.com>
2018-01-13 09:44:46 -0500

    a

--- main.py
+++ main.py
@@ -20,67 +20,15 @@ from rnn import RNNLayer as RNN
 from lstm import LSTMLayer as LSTM
 
 from utils import normalize
+import keras
 
 def main():
     """
     main func
     """
-    
-    text, x, y, char2idx, idx2char = getty()
-    T = 50
-       
-    config = {
-        'dim_hidden' : 200
-      , 'l' : T
-      , 'clip' : 5
-      , 'mu' : 0.9
-      , 'step_size' : 0.001
-    }
 
-    #np.random.seed(42)
-    r = RNN(config)
-    r.accept([27])
-    m = 1
-    ttb = r.sample('f', T * m, char2idx, idx2char, **r.model)
-    r.fit(x[:T * m], y[:T * m ], T, 100000, char2idx, idx2char)
-    tta = r.sample('f', T * m, char2idx, idx2char, **r.model)
-    print(ttb)
-    print(tta)
-    print(text[:T * m])
-    return 
+    print(1)
 
-
-
-
-    (data, label) = cifar()
-    N = 10000
-    data = np.array(data, dtype=float)[:N,]
-    label = np.array(label)[:N,]
-
-    data = normalize(data)
-
-    config = {
-        'input_shape' : [3, 32, 32]
-      , 'mu' : 0.9
-      , 'step_size' : 0.000001
-      , 'step_decay' : 0.95
-    }
-
-    nn = Net(config)
-    conv1 = Conv([3, 3], 6)
-    relu1 = Relu()
-    conv2 = Conv([3, 3], 32)
-    relu2 = Relu()
-    pool = MaxPool()
-    fc = FC([10])
-
-    nn.add(conv1)
-    nn.add(relu1)
-    nn.add(pool)
-    nn.add(fc)
-    
-    print(nn)
-    nn.fit(data, label, 200)
 
 
 if __name__ == "__main__":
