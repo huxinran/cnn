9cdf9d004898345c6999c6d8d0f7b277d692b097
Xinran <huxinran427@gmail.com>
2018-01-13 12:54:27 -0500

    a

---
+++ tf_test.py
@@ -0,0 +1,40 @@
+"""
+main
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from src.net import Net
+from data.data import mnist
+from data.data import cifar
+from data.data import getty
+import sys
+sys.path.append('C:\\Users\\Xinran\\Desktop\\cnn\\src\\')
+sys.path.append('C:\\Users\\Xinran\\Desktop\\cnn\\src\\layer')
+
+plt.ion()
+from fc import FullyConnectedLayer as FC
+from conv import ConvLayer as Conv
+from relu import ReluLayer as Relu
+from maxpool import MaxPoolLayer as MaxPool
+from rnn import RNNLayer as RNN
+from lstm import LSTMLayer as LSTM
+
+#!python
+from utils import normalize
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+import keras
+
+def main():
+    """
+    main func
+    """
+
+
+    
+    print(1)
+    
+
+if __name__ == "__main__":
+    main()
